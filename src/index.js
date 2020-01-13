29787bc69320ba2a12a33e8242047c5b2dc059fd
graeme.cliffe <graeme.cliffe@slalom.com>
2020-01-13 18:58:19 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-/* Your implementation code goes here */
+export class StringCalculator {
+    constructor(){
+    }
+
+    add(str){
+        if (str === ""){
+            return 0;
+        }
+        else {
+            return this.parseValue(str);
+        }
+    }
+
+    parseValue(str){
+        const {delimiter, workStr} = this.getDelimiter(str);
+        let sums = [];
+        workStr.split("\n").forEach(element => {
+            sums.push(...element.split(delimiter));
+        });
+
+        return sums.reduce(
+            function(accumulator, val){
+                let workSum = accumulator + parseInt(val);
+                return workSum;
+            }, 
+            0
+        );
+    }
+
+    getDelimiter(str){
+        let delimiter = ",";
+        let workStr = str;
+        if (str.startsWith("//")){
+            delimiter = str.substring(2, 3);
+            workStr = str.substring(str.indexOf("\n") + 1)
+        } 
+        return {delimiter, workStr};
+    }
+}
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,1 +1,52 @@
 /* your test code goes here. */
+import {StringCalculator} from "./index";
+
+describe('stringCalculator.add', () =>{
+    it('should return 0 if passed empty string', () => {
+        let strCalc = new StringCalculator();
+        let result = strCalc.add("");
+
+        expect(result).toEqual(0);
+    });
+
+    it('should return the parsed value if passed single element', () =>{
+        let strCalc = new StringCalculator();
+        let result = strCalc.add("5");
+        expect(result).toEqual(5);
+    });
+
+    it('should sum two ints when passed as comma separated string', () => {
+        let strCalc = new StringCalculator();
+        let result = strCalc.add("1,2");    
+        
+        expect(result).toEqual(3);
+    });
+
+    it('should sum an unknown number of ints when passed as comma separated string', () => {
+        let strCalc = new StringCalculator();
+        let result = strCalc.add("1,2,5,6,9,33");
+
+        expect(result).toEqual(56);
+    });
+
+    it('should sum an unknown number of ints when passed as new-line separated string', ()=>{
+        let strCalc = new StringCalculator();
+        let result = strCalc.add("1\n3\n8\n4");
+
+        expect(result).toEqual(16)
+    });
+
+    it('should sum a combination of commas and new-line seperated string', () => {
+        let strCalc = new StringCalculator();
+        let result = strCalc.add("1\n2,3");
+
+        expect(result).toEqual(6)
+    });
+
+    it('should be be able to split input by delimiter denoted by //', () => {
+        let strCalc = new StringCalculator();
+        let result = strCalc.add("//;\n1;2");
+
+        expect(result).toEqual(3);  
+    })
+})
